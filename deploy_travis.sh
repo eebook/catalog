aa39b48cc16088dafc35fbffc44d1f857a65c05b
knarfeh <knarfeh@outlook.com>
2017-12-30 12:43:46 +0800

    chore: Rename zhihu2ebook.enc to catalog.enc

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -40,8 +40,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./zhihu2ebook.enc -out ./zhihu2ebook -d
-chmod 600 ./zhihu2ebook
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./catalog.enc -out ./catalog -d
+chmod 600 ./catalog
 eval `ssh-agent -s`
 ssh-add zhihu2ebook
 
